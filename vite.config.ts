1c01e7db020c85048217528b83723749c420117d
Brandon Bybee <bybee.brandon1@gmail.com>
2025-01-31 14:03:03 -0600

    fixed a few bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  define: {
+    "process.env": {},
+  },
 });
